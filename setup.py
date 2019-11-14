a6ecd809d6e837f4e05536e0205b531a13002d98
Jonathan Villemaire-Krajden <jonathan.villemaire-krajden@epfl.ch>
2019-11-14 12:32:08 +0100

    Fix setup.py desc and packages.

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ with open('README.md') as rm:
     long_description = rm.read()
 
 setup(
-    name='clb-oauthenticator',
+    name='clb-authenticator',
     version='0.1.0',
-    description='The collab Authenticator refreshes the user access tokens with the refresh token.',
+    description='The Collaboratory Authenticator is based on the GenericOAuthenticator and adds user refreshing and roles.',
     long_description=long_description,
     author='Human Brain Project Collaboratory Team',
     author_email='support@humanbrainproject.eu',
     url='https://wiki.humanbrainproject.eu/',
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     install_requires=requirements
 )
